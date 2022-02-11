36bd600330e5f79085937c40b9d2b35f04b19636
hugo_gaytan <estardus9@gmail.com>
2022-02-10 21:36:18 -0600

    solucion de problemas con insert

--- ROMADB/ClienteDAL.cs
+++ ROMADB/ClienteDAL.cs
@@ -15,21 +15,20 @@ namespace ROMADB
 
             try
             {
-                string insert = "insert into inventario(nombre_corto, descripcion, serie, color, fecha_adquisicion, " +
-                    "tipo_adquisicion,observaciones, id_areas) values(@nombre,@descripcion,@serie,@color,@fecha,@tipo,@obsercaciones,@id_areas);";
+                string insert = "insert into Inventario(nombre_corto, descripcion, serie, color, fecha_adquisicion, tipo_adquisicion,observaciones, id_areas) values(@nombre,@descripcion,@serie,@color,@fecha,@tipo,@obsercaciones,@ID_areas);";
                 MySqlCommand comando = new MySqlCommand(insert);
 
                 //string fechaHoy = DateTime.Now.ToString("yyyy-MM-dd ");
                 string fechaHoy = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
 
-                comando.Parameters.AddWithValue("?nombre", item.Nombre_corto);
-                comando.Parameters.AddWithValue("?descripcion", item.Descripcion);
-                comando.Parameters.AddWithValue("?serie", item.Serie);
-                comando.Parameters.AddWithValue("?color", item.Color);
-                comando.Parameters.AddWithValue("?fecha", fechaHoy);
-                comando.Parameters.AddWithValue("?tipo", item.Tipo_adquisicion);
-                comando.Parameters.AddWithValue("?obsercaciones", item.Observaciones);
-                comando.Parameters.AddWithValue("?id_areas", item.Id_areas);
+                comando.Parameters.AddWithValue("nombre", item.Nombre_corto);
+                comando.Parameters.AddWithValue("descripcion", item.Descripcion);
+                comando.Parameters.AddWithValue("serie", item.Serie);
+                comando.Parameters.AddWithValue("color", item.Color);
+                comando.Parameters.AddWithValue("fecha", fechaHoy);
+                comando.Parameters.AddWithValue("tipo", item.Tipo_adquisicion);
+                comando.Parameters.AddWithValue("obsercaciones", item.Observaciones);
+                comando.Parameters.AddWithValue("ID_areas", item.Id_areas);
 
                 BDComun.ejecutarSentencia(comando);
 
@@ -50,21 +49,25 @@ namespace ROMADB
             {
                 string select = "SELECT * from Inventario;";
                 MySqlCommand comando = new MySqlCommand(select);
-                BDComun.ejecutarConsulta(comando);
+                
                 //LLENAMOS NUESTRA TABLA 
                 DataTable tabla = BDComun.ejecutarConsulta(comando);
 
                 List<cl_inventario> listaP = new List<cl_inventario>();
+
                 foreach (DataRow d in tabla.Rows)
                 {
                     cl_inventario aux = new cl_inventario();
-                    aux.Nombre_corto = d[0].ToString();
-                    aux.Descripcion = d[1].ToString();
-                    aux.Serie = d[2].ToString();
-                    aux.Color = d[3].ToString();
-                    aux.Fecha_adquisicion = d[3].ToString();
-                    aux.Tipo_adquisicion = d[4].ToString();
-                    aux.Observaciones = d[5].ToString();
+
+                    aux.Id_inventario = Convert.ToInt32(d[0].ToString());
+                    aux.Nombre_corto = d[1].ToString();
+                    aux.Descripcion = d[2].ToString();
+                    aux.Serie = d[3].ToString();
+                    aux.Color = d[4].ToString();
+                    aux.Fecha_adquisicion = d[5].ToString();
+                    aux.Tipo_adquisicion = d[6].ToString();
+                    aux.Observaciones = d[7].ToString();
+                    aux.Id_areas = Convert.ToInt32(d[8].ToString());
 
                     listaP.Add(aux);
                 }
